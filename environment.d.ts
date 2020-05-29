45f3a38cad3d6a24489721ae274f3717751b91ff
jonamat <jo.mataloni@gmail.com>
2020-05-29 19:45:35 +0200

    remove global types errors

---
+++ environment.d.ts
@@ -0,0 +1,23 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production';
+
+            ANALITYCS_ID: string;
+            RECAPTCHA_KEY: string;
+
+            // Contacts
+            PHONE_NUMBER: string;
+            EMAIL: string;
+            TELEGRAM_USER: string;
+            MESSENGER_USER: string;
+            MESSENGER_ID: string;
+            SKYPE_USER: string;
+            SKYPE_ID: string;
+            FACEBOOK_PAGE_NAME: string;
+            INSTAGRAM_PAGE_NAME: string;
+        }
+    }
+}
+
+export {};
